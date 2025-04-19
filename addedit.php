26edf11ebf24626ad26b25146aa6f39cb49c2e74
Paige Julianne Sullivan <psullivan@jhu.edu>
2025-04-18 21:24:44 -0400

    checkin point
    Signed-off-by: Paige Julianne Sullivan <psullivan@jhu.edu>

--- addedit.php
+++ addedit.php
@@ -1,1 +1,67 @@
 <?php
+$displayType = $_REQUEST['displayType']; // either 'project' or 'global'
+
+switch($displayType) {
+    case 'project':
+        require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
+        break;
+    case 'global':
+        require_once APP_PATH_DOCROOT . 'ControlCenter/header.php';
+        break;
+    default:
+        die('You are not allowed to access this page');
+}
+
+
+
+?>
+<h2>Advanced Notifications</h2>
+<h3>Add / Edit Notification</h3>
+<hr/>
+<form method="post">
+<?php
+if ($displayType == "project") {
+    $instruments = REDCap::getInstrumentNames();
+    echo 'Form to apply to:  <select name="instrument">';
+    foreach($instruments as $name => $label) {
+        printf('<option value="%s" %s>%s</option>', $name, $selected, $label);
+    }
+    echo '</select>';
+}
+?><br/><br/>
+    Execute the following actions when this condition is met:<br/>
+    <textarea name="condition" id="code" cols="40" rows="10"></textarea>
+    <br/><br/>
+    <label><input type="checkbox" name="action" value="sendEmail"> Send an email</label><br/>
+    <!-- from -->
+    <!-- to -->
+    <!-- cc -->
+    <!-- bcc -->
+    <!-- subject -->
+    <!-- body -->
+<?php if($displayType == "project"):
+    // need to determine if project has an SMS provider configured
+    ?>
+    <label><input type="checkbox" name="action" value="sendSMS"> Send an SMS message</label><br/>
+    <!-- to -->
+    <!-- body -->
+<?php endif; ?>
+    <label><input type="checkbox" name="action" value="lockForm"> Lock the Form</label><br/>
+<?php if($displayType == "project"): ?>
+    <label><input type="checkbox" name="action" value="setData"> Set record data</label><br/>
+    <!-- growing table with dropdown of variable names on left column, input box on right column -->
+<?php endif; ?>
+    <button class="btn" type="submit">Save</button>
+</form>
+
+<?php
+switch($displayType) {
+    case 'project':
+        require_once APP_PATH_DOCROOT . 'ProjectGeneral/footer.php';
+        break;
+    case 'global':
+        require_once APP_PATH_DOCROOT . 'ControlCenter/footer.php';
+        break;
+    default:
+        die('You are not allowed to access this page');
+}
--- index.php
+++ index.php
@@ -1,2 +1,43 @@
 <?php
-$displayType = $_REQUEST['displayType'];
+$displayType = $_REQUEST['displayType']; // either 'project' or 'global'
+
+switch($displayType) {
+    case 'project':
+        require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
+        break;
+    case 'global':
+        require_once APP_PATH_DOCROOT . 'ControlCenter/header.php';
+        break;
+    default:
+        die('You are not allowed to access this page');
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
+    <h2>Advanced Notifications</h2>
+<hr/>
+
+    <a class="btn" href="<?php echo $module->getUrl('addedit.php?displayType='.$displayType); ?>">Add New Notification</a>
+
+
+
+<?php
+switch($displayType) {
+    case 'project':
+        require_once APP_PATH_DOCROOT . 'ProjectGeneral/footer.php';
+        break;
+    case 'global':
+        require_once APP_PATH_DOCROOT . 'ControlCenter/footer.php';
+        break;
+    default:
+        die('You are not allowed to access this page');
+}
